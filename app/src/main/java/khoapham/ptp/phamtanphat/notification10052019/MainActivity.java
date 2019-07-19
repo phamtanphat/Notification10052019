754fe25e3620b7a63aab0fd1b4b68775c7749dab
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-19 20:43:18 +0700

    a

---
+++ app/src/main/java/khoapham/ptp/phamtanphat/notification10052019/MainActivity.java
@@ -0,0 +1,20 @@
+package khoapham.ptp.phamtanphat.notification10052019;
+
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle(textTitle)
+                .setContentText(textContent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+    }
+}
