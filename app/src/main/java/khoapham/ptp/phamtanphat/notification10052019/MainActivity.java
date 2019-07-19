34a4323999b16bafdc9dc6670ad2cb5542744a38
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-19 21:13:21 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/notification10052019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/notification10052019/MainActivity.java
@@ -14,46 +14,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    NotificationManager mNotificationManager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this.getApplicationContext(), "notify_001");
-        Intent ii = new Intent(this.getApplicationContext(), MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, ii, 0);
-
-        NotificationCompat.BigTextStyle bigText = new NotificationCompat.BigTextStyle();
-        bigText.setBigContentTitle("Today's Bible Verse");
-        bigText.setSummaryText("Text in detail");
-
-        mBuilder.setContentIntent(pendingIntent);
-        mBuilder.setSmallIcon(R.mipmap.ic_launcher_round);
-        mBuilder.setContentTitle("Your Title");
-        mBuilder.setContentText("Your text");
-        mBuilder.setPriority(Notification.PRIORITY_MAX);
-        mBuilder.setStyle(bigText);
-
-        mNotificationManager =
-                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-
-// === Removed some obsoletes
-
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-            String channelId = "Your_channel_id";
-            NotificationChannel channel = new NotificationChannel(
-                    channelId,
-                    "Channel human readable title",
-                    NotificationManager.IMPORTANCE_DEFAULT);
-            mNotificationManager.createNotificationChannel(channel);
-            mBuilder.setChannelId(channelId);
-        }
-
-        mNotificationManager.notify(0, mBuilder.build());
     }
 }
