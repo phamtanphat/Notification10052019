0ad51a53be613642f26cda8d319ed1cb9a5f4a91
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-22 21:14:46 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/notification10052019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/notification10052019/MainActivity.java
@@ -1,14 +1,6 @@
 package khoapham.ptp.phamtanphat.notification10052019;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
